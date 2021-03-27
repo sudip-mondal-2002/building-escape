4b3bcde7961aef520e891016d9c8a683db6ad453
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-03-27 12:38:37 +0530

    inital commit: made a room

---
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -0,0 +1,29 @@
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPosition = (GetOwner()->GetActorLocation()).ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s co-ordinate is %s"), *ObjectName, *ObjectPosition);
+}
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
